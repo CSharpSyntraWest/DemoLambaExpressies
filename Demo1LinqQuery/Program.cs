c90718eb281b1a4e954f8d12693ad80a32243c71
Helena Copppieters <hcoppieters@hotmail.com>
2021-01-21 16:47:30 +0100

    LINQ Query demo1

---
+++ Demo1LinqQuery/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Demo1LinqQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] getallen = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //LINQ Query syntax:
+            var resultKleineGetallen =
+                from getal in getallen
+                where getal <= 3
+                select getal;
+            //LINQ Method syntax:
+            var resultKleineGetallen2 = getallen.Where(getal => getal <= 3).Select(getal =>getal);
+            foreach (int getal in resultKleineGetallen)
+                Console.WriteLine(getal + " ");
+            foreach (int getal in resultKleineGetallen2)
+                Console.WriteLine(getal + " ");
+        }
+    }
+}
--- Demo5LambdaExpressies/Program.cs
+++ Demo5LambdaExpressies/Program.cs
@@ -10,7 +10,7 @@ namespace Demo5LambdaExpressies
             List<string> fruit = new List<string>() { 
                 "appel","peer","citroen","banaan"
             };
-
+            Console.WriteLine("Enkel fruit die letter 'e' bevat:");
             List<string> fruitMetEenE =
                 fruit.FindAll(delegate(string f)
                 {
@@ -21,11 +21,13 @@ namespace Demo5LambdaExpressies
             List<string> fruitMetEenELambda =
                 fruit.FindAll(f => f.Contains("e"));
 
-            fruitMetEenELambda.ForEach(f=> Console.WriteLine(f + " "));
+            fruitMetEenELambda.ForEach(f => Console.WriteLine(f + " "));
             //Oefening:
-            //Geef een lijst terug waarin enkel het fruit zit die eindigen 
-            //op de letter n
-
+            //Geef een lijst terug waarin enkel het fruit zit die eindigt 
+            //op de letter n (met lambda expressie)
+            Console.WriteLine("Enkel fruit die eindigt op 'n':");
+            List<string> fruitMetEenNLambda = fruit.FindAll(f => f.EndsWith("n"));
+            fruitMetEenNLambda.ForEach(f => Console.WriteLine(f + " "));
         }
     }
 }
